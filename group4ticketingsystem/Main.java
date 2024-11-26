d43189b4e544a1fbdc0002afc57caef56decf881
Sel <pianomaster078@gmail.com>
2024-11-26 17:49:03 +0800

    Update Main.java
    NEW (3)

--- group4ticketingsystem/Main.java
+++ group4ticketingsystem/Main.java
@@ -10,7 +10,7 @@ public class Main {
         boolean isRunning = true;
         Scanner scanner = new Scanner(System.in);
         File file = new File("StudentFile.txt");
-        customFilehandler filehandler = new customFilehandler(file);
+        FileHandler fileHandler = new FileHandler(file);
         ArrayList<Student> StudentList = new ArrayList<>();
 
         do{
@@ -30,43 +30,52 @@ public class Main {
 
             switch (option) {
                 case 1:
-                System.out.println("+------------------------------------------------------------------------+");
-                System.out.println("|   NEW RECORD                                                           |");
-                System.out.println("+------------------------------------------------------------------------+");
-                System.out.println();
+                    System.out.println("+------------------------------------------------------------------------+");
+                    System.out.println("|   NEW RECORD                                                           |");
+                    System.out.println("+------------------------------------------------------------------------+");
+                    System.out.println();
 
-                System.out.println("Enter Student Last Name:");
-                String Ltemp = scanner.nextLine();
-                System.out.println("Enter Student First Name:");
-                String Ftemp = scanner.nextLine();
-                String tempfulln = Ftemp + " " + Ltemp;
-                System.out.println("Enter Student Number:");
-                String stunumtemp = scanner.nextLine();
-                System.out.println("Enter Student Course:");
-                String coursetemp = scanner.nextLine();
+                    System.out.print("Enter Student Last Name: ");
+                    String lastName = scanner.nextLine();
+                    System.out.print("Enter Student First Name: ");
+                    String firstName = scanner.nextLine();
+                    String fullName = firstName + " " + lastName;
+                    System.out.print("Enter Student Number (202XXXXXX): ");
+                    String studentNumber = scanner.nextLine();
+                    System.out.print("Enter Student Course: ");
+                    String courseProgram = scanner.nextLine();
+                    System.out.println();
 
-                Student newStudent = new Student(Ltemp, Ftemp, tempfulln, stunumtemp, coursetemp);
-                StudentList.add(newStudent);
+                    Student newStudent = new Student(lastName, firstName, fullName, studentNumber, courseProgram);
+                    StudentList.add(newStudent);
 
-                System.out.println("Do you wanna add an offense?(yes/no)");
-                String offensechoice = scanner.nextLine();
-                while(offensechoice.equalsIgnoreCase("yes")){
-                    System.out.println("Offense Category");
-                    String category = scanner.nextLine();
-                    System.out.println("Enter Offense Degree:");
-                    int degree = scanner.nextInt();
-                    scanner.nextLine();
-                    System.out.println("Enter Offense Details:");
-                    String details = scanner.nextLine();
+                    System.out.print("Do you wanna add an offense [Y|N]: ");
+                    String offensechoice = scanner.nextLine();
+                    System.out.println();
+                    while(offensechoice.equalsIgnoreCase("Y")){
+                        System.out.print("Offense Category: ");
+                        String category = scanner.nextLine();
+                        System.out.print("Enter Offense Degree: ");
+                        int degree = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.print("Enter Offense Details: ");
+                        String details = scanner.nextLine();
 
-                    Offense newOffense = new Offense(category, degree, details);
-                    newStudent.addOffense(newOffense);
+                        Offense newOffense = new Offense(category, degree, details);
+                        newStudent.addOffense(newOffense);
 
-                    System.out.println("Offense added: " + newOffense);
-                    System.out.println("Do you want to add another offense? (yes/no)");
-                    offensechoice = scanner.nextLine();
-                }
+                        System.out.println("Offense added");
+                        System.out.println(newStudent.getStudentDetails());
+                        System.out.println();
+                        System.out.print("Do you want to add another offense [Y|N]: ");
+                        offensechoice = scanner.nextLine();
+                    }
+                    fileHandler.WriteFile(newStudent);
+                    System.out.println("Student details written to file.");
+                    System.out.println("+---------------------------------------------------+");
+                    System.out.println();
                     break;
+                    
 
                 case 2:
                     System.out.println("+------------------------------------------------------------------------+");
@@ -81,7 +90,7 @@ public class Main {
                     System.out.println("+------------------------------------------------------------------------+");
                     System.out.println("");
                     System.out.println("READING...");
-                    filehandler.ReadFile(file);
+                    fileHandler.ReadFile(file);
                     System.out.println("SUCCESSFULLY READ...");
                     System.out.println("+------------------------------------------------------------------------+");
                     System.out.println();
@@ -97,77 +106,5 @@ public class Main {
                     break;
             }   
         }while(isRunning);
-
-
-        // Student student = new Student("Doe", "John", "John Doe", "20231001", "Computer Science");
-        // Offense offense1 = new Offense(1, "Cheating", 2, "Caught cheating during an exam.");
-        // Offense offense2 = new Offense(2, "Plagiarism", 1, "Submitted plagiarized code.");
-        
-        
-        // student.addOffense(offense1);
-        // student.addOffense(offense2);
-
-        
-        // System.out.println(student.getStudentDetails());
-
-        
-        // FileHandler fileHandler = new FileHandler();
-        // File file = new File("StudentFile.txt");
-        
-        
-        // fileHandler.WriteFile(student);
-        // System.out.println("Student details written to file.");
-        // System.out.println();
-        // fileHandler.WriteFile(student);
-        // System.out.println("Student details written to file.");
-        // System.out.println();
-
-        
-        // System.out.println("Reading student details from file:");
-        // fileHandler.ReadFile(file);
-
-        // Student student1 = new Student("Dela Cruz", "John", "John Dela Cruz", "20231001", "Computer Science");
-        // Offense offense3 = new Offense(1, "Bullying", 2, "Bullying one of his collegues.");
-        // Offense offense4 = new Offense(2, "Plagiarism", 1, "Submitted plagiarized code.");
-
-        // student1.addOffense(offense3);
-        // student1.addOffense(offense4);
-
-        // System.out.println(student.getStudentDetails());
-
-        // FileHandler fileHandler1 = new FileHandler();
-        // File file1 = new File("StudentFile.txt");
-        
-        // fileHandler1.WriteFile(student1);
-        // System.out.println("Student details written to file.");
-        // System.out.println();
-
-        
-        // System.out.println("Reading student details from file:");
-        // fileHandler1.ReadFile(file1);
-
-        // System.out.println("Reading student details from file:");
-        // fileHandler.ReadFile(file);
-
-        // Student student1 = new Student("Dela Cruz", "John", "John Dela Cruz", "20231001", "Computer Science");
-        // Offense offense3 = new Offense(1, "Bullying", 2, "Bullying one of his collegues.");
-        // Offense offense4 = new Offense(2, "Plagiarism", 1, "Submitted plagiarized code.");
-
-        // student1.addOffense(offense3);
-        // student1.addOffense(offense4);
-
-        // System.out.println(student.getStudentDetails());
-
-        // FileHandler fileHandler1 = new FileHandler();
-        // File file1 = new File("StudentFile.txt");
-        
-        // fileHandler1.WriteFile(student1);
-        // System.out.println("Student details written to file.");
-        // System.out.println();
-
-        
-        // System.out.println("Reading student details from file:");
-        // fileHandler1.ReadFile(file1);
-
     }
 }
