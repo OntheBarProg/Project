e147577bc286da7d2b9f26a7c5be560cf4c5d501
Sel <pianomaster078@gmail.com>
2024-11-27 23:07:36 +0800

    Update Main.java
    NEW

--- group4ticketingsystem/Main.java
+++ group4ticketingsystem/Main.java
@@ -1,114 +1,5 @@
 package group4ticketingsystem;
 
-
-// import java.util.*;
-// import java.io.*;
-
-// public class Main {
-//     public static void main(String[] args) {
-//         int option;
-//         boolean isRunning = true;
-//         Scanner scanner = new Scanner(System.in);
-//         File file = new File("StudentFile.txt");
-//         FileHandler fileHandler = new FileHandler(file);
-//         ArrayList<Student> StudentList = new ArrayList<>();
-
-//         do{
-//             System.out.println("+---------------------------------------------------+");
-//             System.out.println("|                       MENU                        |");
-//             System.out.println("+---------------------------------------------------+");
-//             System.out.println("|  [1] - ADD RECORD                                 |");
-//             System.out.println("|  [2] - SEARCH RECORDS                             |");
-//             System.out.println("|  [3] - VIEW RECORDS                               |");
-//             System.out.println("|  [4] - EXIT                                       |");
-//             System.out.println("+---------------------------------------------------+");
-//             System.out.print("OPTION: ");
-//             option = scanner.nextInt();
-//             scanner.nextLine();
-//             System.out.println("+---------------------------------------------------+");
-//                 System.out.println();
-
-//             switch (option) {
-//                 case 1:
-//                     System.out.println("+------------------------------------------------------------------------+");
-//                     System.out.println("|   NEW RECORD                                                           |");
-//                     System.out.println("+------------------------------------------------------------------------+");
-//                     System.out.println();
-
-//                     System.out.print("Enter Student Last Name: ");
-//                     String lastName = scanner.nextLine();
-//                     System.out.print("Enter Student First Name: ");
-//                     String firstName = scanner.nextLine();
-//                     String fullName = firstName + " " + lastName;
-//                     System.out.print("Enter Student Number (202XXXXXX): ");
-//                     String studentNumber = scanner.nextLine();
-//                     System.out.print("Enter Student Course: ");
-//                     String courseProgram = scanner.nextLine();
-//                     System.out.println();
-
-//                     Student newStudent = new Student(lastName, firstName, fullName, studentNumber, courseProgram);
-//                     StudentList.add(newStudent);
-
-//                     System.out.print("Do you wanna add an offense [Y|N]: ");
-//                     String offensechoice = scanner.nextLine();
-//                     System.out.println();
-//                     while(offensechoice.equalsIgnoreCase("Y")){
-//                         System.out.print("Offense Category: ");
-//                         String category = scanner.nextLine();
-//                         System.out.print("Enter Offense Degree: ");
-//                         int degree = scanner.nextInt();
-//                         scanner.nextLine();
-//                         System.out.print("Enter Offense Details: ");
-//                         String details = scanner.nextLine();
-
-//                         Offense newOffense = new Offense(category, degree, details);
-//                         newStudent.addOffense(newOffense);
-
-//                         System.out.println("Offense added");
-//                         System.out.println(newStudent.getStudentDetails());
-//                         System.out.println();
-//                         System.out.print("Do you want to add another offense [Y|N]: ");
-//                         offensechoice = scanner.nextLine();
-//                     }
-//                     fileHandler.WriteFile(newStudent);
-//                     System.out.println("Student details written to file.");
-//                     System.out.println("+---------------------------------------------------+");
-//                     System.out.println();
-//                     break;
-                    
-
-//                 case 2:
-//                     System.out.println("+------------------------------------------------------------------------+");
-//                     System.out.println("|   SEARCH                                                               |");
-//                     System.out.println("+------------------------------------------------------------------------+");
-//                     System.out.println("");
-//                     break;
-
-//                 case 3:
-//                     System.out.println("+------------------------------------------------------------------------+");
-//                     System.out.println("|   VIEW                                                                 |");
-//                     System.out.println("+------------------------------------------------------------------------+");
-//                     System.out.println("");
-//                     System.out.println("READING...");
-//                     fileHandler.ReadFile(file);
-//                     System.out.println("SUCCESSFULLY READ...");
-//                     System.out.println("+------------------------------------------------------------------------+");
-//                     System.out.println();
-//                     break;
-
-//                 case 4:
-//                     System.out.println("EXIT...");
-//                     isRunning = false;
-//                     break;
-            
-//                 default:
-//                     System.out.println("INVALID OPTION...");
-//                     break;
-//             }   
-//         }while(isRunning);
-//     }
-// }
-
 import java.util.*;
 import java.io.*;
 
@@ -118,14 +9,14 @@ public class Main {
     private File file = new File("StudentFile.txt");
     private FileHandler filehandler = new FileHandler(file);
     
-    public Main(ArrayList<Student> StudentList, Scanner scanner, File file, FileHandler filehandler){
+    public Main(ArrayList<Student> StudentList, Scanner scanner, File file, FileHandler filehandler) {
         this.StudentList = StudentList;
         this.scanner = scanner;
         this.file = file;
         this.filehandler = filehandler;
     }
     
-    void main(){
+    void main() {
         System.out.println("+---------------------------------------------------+");
         System.out.println("|                       MENU                        |");
         System.out.println("+---------------------------------------------------+");
@@ -161,10 +52,56 @@ public class Main {
         String offensechoice = scanner.nextLine();
         System.out.println();
         while(offensechoice.equalsIgnoreCase("Y")){
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println("|   OFFENSES                                                             |");
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println("|   ACADEMICS                                                            |");
+            System.out.println("|   C       -   Cheating                                                 |");
+            System.out.println("|   IDOAM   -   Illegal Distribution of Academic Materials               |");
+            System.out.println("|   MOAM    -   Misuse of Academic Materials                             |");
+            System.out.println("|   P       -   Plagiarism                                               |");
+            System.out.println("|                                                                        |");
+            System.out.println("|   SOCIAL                                                               |");
+            System.out.println("|   B       -   Bullying                                                 |");
+            System.out.println("|   CB      -   Cyber Bullying                                           |");
+            System.out.println("|   SA      -   Sexual Assault                                           |");
+            System.out.println("|                                                                        |");
+            System.out.println("|   INSTITUTIONAL                                                        |");
+            System.out.println("|   POF     -   Pocession of Fire Arms                                   |");
+            System.out.println("|   S|V     -   Smoking | Vaping                                         |");
+            System.out.println("|   DOSP    -   Destruction of School Property                           |");
+            System.out.println("|   IWOIU   -   Improper Wearing of Institutional Uniform [M|TH]         |");
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println();
             System.out.print("Offense Category: ");
             String category = scanner.nextLine();
+            System.out.println();
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println("|   DEGREE OF OFFENSE                                                    |");
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println("| LEVEL | SEVERITY     | DESCRIPTION                                     |");
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println("|  1    | Minor        | Minor infractions causing minimal harm or       |");
+            System.out.println("|       |              | disruption. Typically results in a warning.     |");
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println("|  2    | Moderate     | Infractions causing mild harm or disruption.    |");
+            System.out.println("|       |              | Require formal warnings or corrective actions.  |");
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println("|  3    | Significant  | Serious offenses negatively impacting others.   |");
+            System.out.println("|       |              | Result in disciplinary action.                  |");
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println("|  4    | Severe       | Severe offenses causing serious harm or         |");
+            System.out.println("|       |              | major breaches of rules. May lead to legal      |");
+            System.out.println("|       |              | actions or suspension.                          |");
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println("|  5    | Critical     | Extremely serious offenses causing substantial  |");
+            System.out.println("|       |              | harm. Often result in expulsion or criminal     |");
+            System.out.println("|       |              | prosecution.                                    |");
+            System.out.println("+------------------------------------------------------------------------+");
+            System.out.println();
             System.out.print("Enter Offense Degree: ");
             int degree = scanner.nextInt();
+            System.out.println();
             scanner.nextLine();
             System.out.print("Enter Offense Details: ");
             String details = scanner.nextLine();
@@ -185,7 +122,10 @@ public class Main {
     }
     
     void searchRecords(){
-        //TODO 
+        System.out.println("+------------------------------------------------------------------------+");
+        System.out.println("|   SEARCH                                                               |");
+        System.out.println("+------------------------------------------------------------------------+");
+        System.out.println(""); 
     }
     
     void viewRecords () {
